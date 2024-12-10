e2ad192cc7dfa3833baa060b2aba01a8c77ed9c6
Laxman KC <laxmankc@Laxmans-MacBook-Pro.local>
2024-12-10 14:56:08 -0500

    Remove ignored files from tracking

--- obj/Debug/net9.0-ios/iossimulator-arm64/nativelibraries/aot-output/arm64/System.Private.CoreLib.dll.s
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:445e916af8d3640d0d11033960d282e489ecfc4482269aa66431603a3fdd9458
-size 1256214
--- obj/Debug/net9.0-ios/iossimulator-arm64/nativelibraries/aot-output/arm64/aot-instances.dll.s
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1713a82b64ded0ae95d43e49538a337e07df58ed59f4840ddecb14239d0622b9
-size 151735788
