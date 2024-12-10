e85527c264e27c4c14f545f8dd21815706e2184f
Laxman KC <laxmankc@Laxmans-MacBook-Pro.local>
2024-12-10 14:55:55 -0500

    Add .gitignore for MAUI project

---
+++ obj/Debug/net9.0-maccatalyst/maccatalyst-arm64/nativelibraries/aot-output/arm64/System.Private.CoreLib.dll.s
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:59c888a8e43178aa6ae0ba0f6ffdbf1b824730936914515048aa4254364b82ba
+size 1233288
---
+++ obj/Debug/net9.0-maccatalyst/maccatalyst-arm64/nativelibraries/aot-output/arm64/aot-instances.dll.s
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:69de2c4b561f460061295fa6a5c60d3a572daf379345413e1767e91648d32ae2
+size 154763901
